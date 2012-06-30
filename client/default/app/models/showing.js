12200bc475e2aebc215b41bf4a53c59897ea5170
Alan Moran <alan@alanmoran.me>
2012-06-30 19:08:45 +0100

    Updaing showing model to do correct act call

---
+++ client/default/app/models/showing.js
@@ -0,0 +1,27 @@
+var showing={
+	data:[],
+	load:function(callback){
+		var that=this;
+		$fh.act({
+			"act":"getXML",
+			"secure":false
+		},function(res){
+			
+			if (res && res.data){
+				that.data=res.data;
+				if (callback){
+					callback(res.data);
+				}
+			}
+		})
+	},
+	getData:function(callback){
+		if (this.data===[]){
+			this.load(callback);
+		}else{
+			if (callback){
+				callback(this.data);
+			}
+		}
+	}
+}
